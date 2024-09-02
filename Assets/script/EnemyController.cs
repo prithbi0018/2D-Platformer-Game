ba28d2be12a56d2e09d29b03eb469afff91a6f6b
prithbi0018 <prithbirajpanda@gmail.com>
2024-09-02 11:56:23 +0530

    added play and quit button

---
+++ Assets/script/EnemyController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Animator enemyAnimator;
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private GameObject groundDetector;
+    [SerializeField] private float rayDistance;
+    [SerializeField] private Transform leftBoundary;
+    [SerializeField] private Transform rightBoundary;
+
+    private int directionChanger = 1;
+
+    private void Update()
+    {
+        patrolEnemy();
+    }
+
+    private void patrolEnemy()
+    {
+        enemyAnimator.SetBool("IsPatrol", true);
+        transform.Translate(directionChanger * Vector2.right * moveSpeed * Time.deltaTime);
+
+        RaycastHit2D hit = Physics2D.Raycast(groundDetector.transform.position, Vector2.down, rayDistance);
+        if (transform.position.x <= leftBoundary.position.x || transform.position.x >= rightBoundary.position.x)
+        
+        {
+            Vector3 scaleVector = transform.localScale;
+            scaleVector.x *= -1;
+            transform.localScale = scaleVector;
+            directionChanger *= -1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the colliding object has a PlayerMovement component
+        PlayerMovement playerMovement = collision.gameObject.GetComponent<PlayerMovement>();
+        if (playerMovement != null)
+        {
+            playerMovement.TakeDamage(); // Call TakeDamage on PlayerMovement
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            return;
+        }
+
+        // Handle collision with other objects (e.g., walls, player)
+        Vector3 scaleVector = transform.localScale;
+        scaleVector.x *= -1;
+        transform.localScale = scaleVector;
+        directionChanger *= -1;
+
+    }
+}
